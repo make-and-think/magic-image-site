c1e5f991e84cde8ceb38bce264df3449de149597
Taruu <taruumax@gmail.com>
2024-10-29 16:38:53 +0300

    some littel functions

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,27 @@
-import { Title } from "@solidjs/meta";
+import {Title} from "@solidjs/meta";
 import Counter from "~/components/Counter";
 import {FileSelector} from "~/components/FileSelector";
 import {createSignal, For} from "solid-js";
 
 export default function Home() {
-    const [get_files, set_files] = createSignal<File[]>([]);
+    const [get_selectedFiles, set_selectedFiles] = createSignal<File[]>([]);
+    const add_selectedFiles = (selectedFilesToAdd: File[]) => {
+        set_selectedFiles(get_selectedFiles().concat(selectedFilesToAdd))
+    };
 
 
 
-  return (
-    <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-     <FileSelector set_files={(files) => {
-         const uniFiles = new Set(get_files().concat(files))
-         set_files([...uniFiles])}} />
-    <li>
-        <For each={get_files()}>
-            {(file, i) => (<ol>{file.name}</ol>)}
-        </For>
-    </li>
-    </main>
-  );
+
+    return (
+        <main>
+            <Title>Hello World</Title>
+            <h1>Hello world!</h1>
+            <FileSelector set_files={add_selectedFiles}/>
+            <li>
+                <For each={get_selectedFiles()}>
+                    {(file, i) => (<ol>{file.name}</ol>)}
+                </For>
+            </li>
+        </main>
+    );
 }
